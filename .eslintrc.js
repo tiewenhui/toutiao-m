7bf09e74148afdacbf6514e3321018a6fa812f1e
tiewenhui <tiewenhui@qq.com>
2020-08-01 23:55:22 +0800

    项目初始化-调整初始目录结构

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,19 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "space-before-blocks": 0,
+    indent: 0,
+    "keyword-spacing": 0,
+    quotes: 0,
+    "eol-last": 0,
+    "key-spacing": 0,
+    "space-before-function-paren": 0
   }
 }
